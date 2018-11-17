7d6736f080c8a265738bb686b890eeebb09f5590
Michael Borde <michael.borde@ubimail.fr>
2018-11-17 09:54:15 +0100

    webpack simplification

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,8 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-const mode = process.env.NODE_ENV || 'development';
-const production = mode === 'production';
 
 const configuration = {
-  mode,
-  devtool: production ? 'inline-source-map' : 'none',
   entry: { app: resolve('app', 'index.ts') },
-  output: {
-    path: resolve('build'),
-    filename: production ? '[name].[hash].bundle.js' : '[name].bundle.js'
-  },
   resolve: {
     extensions: ['.ts', '.js']
   },
@@ -40,10 +29,8 @@ const configuration = {
   plugins: [
     new HtmlWebpackPlugin({
       template: resolve('app', 'index.html')
-    }),
-    production ? new webpack.NoEmitOnErrorsPlugin() : null,
-    production ? new UglifyJsPlugin({ sourceMap: true }) : null
-  ].filter(x => x)
+    })
+  ]
 };
 
 function resolve(...paths) {
